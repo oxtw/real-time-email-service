8253086649d1ab24d1651851db945515bf618c5b
miguel <weigertmiguel@hotmail.com>
2024-11-29 16:12:14 -0300

    cadastrando-usuario

--- frontend/src/app/signup/page.tsx
+++ frontend/src/app/signup/page.tsx
@@ -1,7 +1,35 @@
 import styles from "../page.module.scss";
 import Link from "next/link";
+import { api } from "@/services/api";
+import { redirect } from "next/navigation";
 
 export default function Signup() {
+  async function handleRegister(formData: FormData) {
+    "use server";
+
+    const username = formData.get("username");
+    const email = formData.get("email");
+    const password = formData.get("password");
+
+    if (username === "" || email === "" || password === "") {
+      console.log("PREENCHA TODOS OS CAMPOS");
+      return;
+    }
+
+    try {
+      await api.post("/users",{
+        username,
+        email,
+        password,
+      });        
+    } catch (err) {
+      console.log("error");
+      console.log(err);
+    }
+
+    redirect("/")
+  }
+
   return (
     <>
       <div className={styles.containerCenter}>
@@ -9,9 +37,9 @@ export default function Signup() {
 
         <section className={styles.login}>
           <h1>Criando sua conta</h1>
-          <form action="">
+          <form action={handleRegister}>
             <input
-              type="username"
+              type="text"
               required
               name="username"
               placeholder="Digite seu nome de usuario..."
